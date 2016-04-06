f46a7473be1417b64460d4f5d65c594d3524ba1b
scq000 <scq000@hotmail.com>
2016-04-06 17:03:52 +0800

    add demo4 --- learn about multiple config files

---
+++ demo4/webpack.autoprefixer.config.js
@@ -0,0 +1,23 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	context: __dirname + '/src',
+	entry: './',
+	module: {
+		loaders: [{
+			test: /\.css$/,
+			include: [
+				__dirname + '/src'
+			],
+			loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader')
+		}]
+	},
+	output: {
+		path: __dirname + '/dist',
+		filename: '[name].bundle.[hash].js'
+	},
+	plugins: [
+		new ExtractTextPlugin('[name].css')
+	]
+
+}
